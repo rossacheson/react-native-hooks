471a300bfe696611d2ffa40aedcbcca732154e0b
Ross Acheson <rossacheson@users.noreply.github.com>
2021-12-08 09:54:24 -0500

    useState example

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Touchable,
+} from 'react-native';
 
 const TodoList = () => {
-  const todos = [
+  const [todos, setTodos] = useState([
     { text: 'Pay bills', id: '1' },
     { text: 'Do your homework', id: '2' },
     { text: 'Walk the dog', id: '3' },
-  ];
+  ]);
+
+  const addTodo = () => {
+    setTodos([...todos, { text: 'Learn Hooks', id: `${Math.random()}` }]);
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -16,6 +28,9 @@ const TodoList = () => {
         }}
         keyExtractor={(todo) => todo.id}
       />
+      <TouchableOpacity onPress={addTodo} style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Add Todo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -23,6 +38,15 @@ const TodoList = () => {
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
+  },
+  buttonContainer: {
+    backgroundColor: 'dodgerblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
   },
 });
 
